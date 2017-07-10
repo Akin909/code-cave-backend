ee182d3839503936ea9cd999f9244075ff510a77
Akin909 <akin.sowemimo@gmail.com>
2017-07-10 13:09:19 +0100

    fix syntax error in connection

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -5,13 +5,13 @@ const connection = {
   host: process.env.HEROKU_HOST,
   user: process.env.HEROKU_USER,
   password: process.env.HEROKU_PW,
-  database: process.env.HEROKU_DB
+  database: process.env.HEROKU_DB,
+  ssl: true
   //host: 'ec2-23-23-225-12.compute-1.amazonaws.com',
   //port: 5432,
   //user: 'zsjbvcklijjzdx',
   //password: 'fe8f0ebfb82c2089887522ba9532b51ce1ae4005b2707f63d54b75097ec72b7d',
   //database: 'db7tvip5ooa815',
-  ssl: true
 };
 
 const db = pgp(connection);
