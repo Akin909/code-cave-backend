97cbe4e5a20f0900a007538526e29fc9750115a7
Akin909 <akin.sowemimo@gmail.com>
2017-07-10 13:08:05 +0100

    add heroku env variables to connection

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,11 +2,15 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const connection = {
-  host: 'ec2-23-23-225-12.compute-1.amazonaws.com',
-  port: 5432,
-  user: 'zsjbvcklijjzdx',
-  password: 'fe8f0ebfb82c2089887522ba9532b51ce1ae4005b2707f63d54b75097ec72b7d',
-  database: 'db7tvip5ooa815',
+  host: process.env.HEROKU_HOST,
+  user: process.env.HEROKU_USER,
+  password: process.env.HEROKU_PW,
+  database: process.env.HEROKU_DB
+  //host: 'ec2-23-23-225-12.compute-1.amazonaws.com',
+  //port: 5432,
+  //user: 'zsjbvcklijjzdx',
+  //password: 'fe8f0ebfb82c2089887522ba9532b51ce1ae4005b2707f63d54b75097ec72b7d',
+  //database: 'db7tvip5ooa815',
   ssl: true
 };
 
