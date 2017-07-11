c135e13779901cc41357efeb704f5a3b21555700
Akin909 <akin.sowemimo@gmail.com>
2017-07-11 12:44:34 +0100

    add prod env for heroku and cors if local

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ import { schema } from './graphql/schema.js';
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+console.log('process env', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use('*', cors({ origin: 'http://localhost:8080' }));
+}
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
