2f9ad7d22068a91b3e1125a2403c7e25a74d153c
Akin909 <akin.sowemimo@gmail.com>
2017-07-11 12:37:19 +0100

    allow cors in express server file as per docs

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ import { schema } from './graphql/schema.js';
 const app = express();
 const port = process.env.PORT || 4001;
 
-app.use('*', cors({ origin: 'http://localhost:8080' }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
