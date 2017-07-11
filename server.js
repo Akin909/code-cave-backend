e90e12dc70087a420755456c246b4ee241689a86
Akin909 <akin.sowemimo@gmail.com>
2017-07-11 12:46:34 +0100

    revert package json changes and enable cors for all routes

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ import { schema } from './graphql/schema.js';
 const app = express();
 const port = process.env.PORT || 4001;
 
-console.log('process env', process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development') {
-  app.use('*', cors({ origin: 'http://localhost:8080' }));
-}
+app.use(cors());
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
